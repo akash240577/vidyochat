aabe8bc280ec05c402710892f50d0bd15d858a0b
arajput <arajput@getwellnetwork.com>
2018-09-06 12:01:46 -0400

    AR - initial cleanup

--- src/app/sn-directives/join-vidyo-call.directive.ts
+++ src/app/sn-directives/join-vidyo-call.directive.ts
@@ -1,7 +1,9 @@
 declare function generateToken(key, appID, userName, expiresInSeconds, vcard);
+
 declare var vidyoConnector: any;
 
-import { OnInit, Directive, ElementRef, Renderer, HostListener, style } from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, style} from '@angular/core';
+import * as jsSHA from "jssha";
 
 @Directive({
   selector: '[appJoinVidyoCall]',
@@ -15,15 +17,17 @@ export class JoinVidyoCallDirective {
   public expiresInSeconds = 86400;
   public userInfo: any;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
+  }
+
   @HostListener('click', ['$event']) onclick($event) {
-    this.userInfo = this.el.nativeElement.getAttribute('data-userinfo').toString().split('|');    
+    this.userInfo = this.el.nativeElement.getAttribute('data-userinfo').toString().split('|');
     if (this.userInfo.length == 2) {
       this.userName = this.userInfo[0].toString();
       this.roomName = this.userInfo[1].toString();
     }
 
-    var generateTokenValue = generateToken(this.key, this.appID, this.userName, this.expiresInSeconds, "");
+    const generateTokenValue = generateToken(this.key, this.appID, this.userName, this.expiresInSeconds, "");
 
     if (this.userName != "" && this.roomName != "") {
       vidyoConnector.Connect({
@@ -45,5 +49,19 @@ export class JoinVidyoCallDirective {
     else {
       $(".sn-vidyo-user-details").fadeIn();
     }
+  }
+
+  generateToken(key, appID, userName, expiresInSeconds, vCard) {
+    var EPOCH_SECONDS = 62167219200;
+    var expires = Math.floor(Date.now() / 1000) + expiresInSeconds + EPOCH_SECONDS;
+    var shaObj = new jsSHA("SHA-384", "TEXT");
+    shaObj.setHMACKey(key, "TEXT");
+    var jid = userName + '@' + appID;
+    var body = 'provision' + '\x00' + jid + '\x00' + expires + '\x00' + vCard;
+    shaObj.update(body);
+    var mac = shaObj.getHMAC("HEX");
+    var serialized = body + '\0' + mac;
+    console.log("\nGenerated Token: \n" + btoa(serialized));
+    return btoa(serialized);
   }
 }
