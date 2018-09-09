2509607c511c8541a4692db685df46f0ca51adcf
arajput <arajput@getwellnetwork.com>
2018-09-08 20:03:06 -0400

    Auto-generated commit

---
+++ assets/scripts/VidyoClientLoad.js
@@ -0,0 +1,34 @@
+var vidyoConnector;
+function onVidyoClientLoaded(status) {
+    switch (status.state) {
+        case "READY":
+            VC.CreateVidyoConnector({
+                viewId: "connected-video-chat",
+                viewStyle: "VIDYO_CONNECTORVIEWSTYLE_Default",
+                remoteParticipants: 10,
+                logFileFilter: "warning all@VidyoConnector info@VidyoClient",
+                logFileName: "",
+                userData: ""
+            }).then(function (vc) {
+                console.log("VidyoClient Success", vc);
+                vidyoConnector = vc;
+            }).catch(function (error) {
+                console.log("VidyoClient failed", error);
+                onVidyoClientLoaded(status);
+            });
+            break;
+        case "RETRYING": // The library operating is temporarily paused
+            console.log("RETRYING");
+            break;
+        case "FAILED": // The library operating has stopped
+            console.log("FAILED");
+            break;
+        case "FAILEDVERSION":
+            console.log("FAILEDVERSION");
+            break;
+        case "NOTAVAILABLE": // The library is not available
+            console.log("NOTAVAILABLE");
+            break;
+    }
+    return true; // Return true to reload the plugins if not available
+}
